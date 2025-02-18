9cef5ddc2a8fbf5897f8f99d845ed3e66b556a1b
francisco <pacho10futbol@gmail.com>
2025-02-18 10:34:29 -0500

    implementacion de la primera clase y sus metodos

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,20 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+from datetime import datetime
+
+class Note:
+    HIGH: str = "HIGH"
+    MEDIUM: str = "MEDIUM"
+    LOW: str = "LOW"
+
+    def __init__(self, code: str, title: str, text: str, importance: str):
+        self.code: str = code
+        self.title: str= title
+        self.text: str = text
+        self.importance: str = importance
+        self.creation_date = datetime.now()
+        tags: list[str] = list()
+
+    def add_tag(self, tag: str):
+
+
+
